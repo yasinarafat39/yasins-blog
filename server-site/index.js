3b9f479b67adbbf716d8290c189e48b6a679339c
Yasin Arafat <yasinarafat2557@gmail.com>
2024-02-04 11:45:06 +0600

    connected our server to MongoDB database

--- server-site/index.js
+++ server-site/index.js
@@ -1,15 +1,33 @@
-const express = require('express'); // import express.js 
 
+require("dotenv").config(); // environment variable
+
+// required packages 
+const express = require('express'); // import express.js 
+const mongoose = require("mongoose");
+
+
+// initialize express
 const app = express(); // Create an instance of an Express application 
+
+
+// mongodb connect
+mongoose.connect(
+    process.env.MONGODB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+    .then(() => {
+        console.log("Mongodb is connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+
 
 // Define a basic route 
 app.get('/', (req, res) => {
     res.send("Hello, Yasin's world!"); // send a response
-});
-
-// new route about yasin's blog
-app.get('/about', (req, res) => {
-    res.send("This is about from Yasin's blog Server inside.");
 });
 
 
